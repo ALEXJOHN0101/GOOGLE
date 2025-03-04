c53b93270ca69b02326e07ac3e3ac42fd8b34e21
ALEXJOHN0101 <144509648+ALEXJOHN0101@users.noreply.github.com>
2025-03-04 12:21:35 +0530

    Add files via upload

---
+++ alex 4 march.R
@@ -0,0 +1,33 @@
+a=8
+b=5
+c=a*b
+c
+"hello world"
+x<- c(5,-8,2,6,1)
+x2<- c(5,-8,2,6,1)
+x
+x+2
+x*2
+mean(x)
+median(x)
+sum((x-mean(x))^2)
+1:10
+seq(from=-100,to=100,by=20)
+seq(from=-100,to=100,length=20)
+rep('Alex',4)
+rep(4:10,each=5)
+rep(-1:3,length.out=100)
+2^(0:10)
+1:10*c(-1,1)
+1:10*c(-1,1)
+1:3*3:1
+x<-c(5,7,8,9)
+x[4]
+x[c(2,4,1)]
+x<-c("hello","how do u do ","very bad",42)
+x
+x[2]
+x[3]
+c(x[1:2],"goodbye")
+x[-2]
+matrix(1:12,nrow=3,ncol=4)
